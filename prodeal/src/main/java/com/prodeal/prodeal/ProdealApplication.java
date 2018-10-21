abb8e8403da6c6110e47a65995eb60868e327ab6
Ez-zaidi <soufiane.ez.zaidi@gmail.com>
2018-10-22 01:03:16 +0200

    New Config

---
+++ prodeal/src/main/java/com/prodeal/prodeal/ProdealApplication.java
@@ -0,0 +1,12 @@
+package com.prodeal.prodeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdealApplication.class, args);
+	}
+}
